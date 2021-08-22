475ae31239d39fa845a0c8d6895fa67ee13d2fb1
AlexStep16 <senior.alexander2016@yandex.ru>
2021-08-22 18:32:56 +0300

    PROD add mail sender

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\RecoveryPasswordMail;
 use App\Models\User;
+use App\Models\Token;
+use Carbon\Carbon;
 
 class RegisterController extends Controller
 {
@@ -24,17 +28,40 @@ class RegisterController extends Controller
     public function restorePassword(Request $request) {
       $model = User::where('email', $request->email)->first();
       if($model !== null) {
-        return response('Success', 200);
+        $timestamp = Carbon::now()->timestamp;
+        $hash = Hash::make($request->email . $timestamp);
+        $tokenModel = Token::where('email', $request->email)->first();
+        if(!is_null($tokenModel)) {
+          if($tokenModel->wasActivated === false) {
+            $token = $tokenModel->token;
+          }
+          else {
+            $token = Token::create(['token' => $hash, 'email' => $request->email, 'wasActivated' => false]);
+          }
+        }
+        else {
+          $token = Token::create(['token' => $hash, 'email' => $request->email, 'wasActivated' => false]);
+        }
+        if($token) {
+          Mail::to($request->email)->send(new RecoveryPasswordMail($hash));
+          return response('Success', 200);
+        }
+        else {
+          return response('Fail', 500);
+        }
       }
       else {
-        return response('Fail', 404);
+        return response('Fail', 500);
       }
     }
 
     public function changePassword(Request $request) {
-      $model = User::where('hash', $request->hash)->first();
+      $tokenModel = Token::where('token', $request->hash)->first();
+      if(!is_null($tokenModel)) $email = $tokenModel->email;
+      else $email = null;
+      $model = User::where('email', $email)->first();
       if($model === null) {
-        return response('Fail', 404);
+        return response("Fail $tokenModel", 500);
       }
 
       $model->password = Hash::make($request->password);
---
+++ resources/views/recovery-password.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+  <div style="text-align:center;"><img src="http://skyber.loc/storage/skyber.jpg"></div>
+  <div style="width: 100%;height: auto;border: 2px solid #6669fb;font-family: 'Arial', sans-serif;border-radius: 2px ">
+    <h2 style="font-size: 1em;padding:13px;margin:0;background: #6669fb;color: #fff;">Восстановление пароля</h2>
+    <p style="margin:0;padding:34px 13px;font-size: 1.05em;color:#444">Был получен запрос на восстановление пароля для этого E-Mail адреса. <a href="https://skyber.ru/password/change/{{ $hash }}" target="_blank">Нажмите сюда чтобы изменить пароль</a></p>
+  </div>
+</body>
+</html>
